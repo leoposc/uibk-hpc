d48e23eb9a73de2970e9ce9aed056c1f5b8c8c1d
Peter Burger <peter.burger@student.uibk.ac.at>
2023-10-15 21:55:53 +0200

    minor improvements

--- week02/exercise02/benchmark.sh
+++ week02/exercise02/benchmark.sh
@@ -2,7 +2,7 @@
 
 # define the parameters
 ns=(256 512 1024 2048 4096 8192)
-rs=(1 2 4 8 16 32 64)
+rs=(1 2 4 8 16 32 64 128)
 
 # load the modules
 module purge
@@ -12,7 +12,6 @@ module load openmpi/3.1.6-gcc-12.2.0-d2gmn55
 for n in "${ns[@]}"
 do
     echo "executing seq-$n"
-    export SLURM_OUTPUT="seq-$n.txt"
     sbatch --wait ./seq-job-script.slurm $n
 done
 
@@ -22,8 +21,6 @@ do
     for r in "${rs[@]}"
     do
         echo "executing mpi-$n-$r"
-        export SLURM_NTASKS=$r
-        export SLURM_OUTPUT="mpi-$n-$r.txt"
         sbatch --wait ./mpi-job-script.slurm $n $r
     done
 done
--- week02/exercise02/heat_stencil_1D_seq.c
+++ week02/exercise02/heat_stencil_1D_seq.c
@@ -97,10 +97,8 @@ int main(int argc, char **argv) {
   printf("\n");
 
   int success = 1;
-  double hash = 0.0;
   for (long long i = 0; i < N; i++) {
     value_t temp = A[i];
-    hash += temp;
     if (273 <= temp && temp <= 273 + 60)
       continue;
     success = 0;
@@ -108,7 +106,6 @@ int main(int argc, char **argv) {
   }
 
   printf("Verification: %s\n", (success) ? "OK" : "FAILED");
-  printf("Hash: %lf\n", hash);
   printf("Time: %lfs\n", (t_stop - t_start) / (double) CLOCKS_PER_SEC);
 
   // ---------- cleanup ----------
