8b33671854313089796a806d52b3c7cbdd978a6b
Peter Burger <peter.burger@student.uibk.ac.at>
2023-10-22 22:34:53 +0200

    Revert "reduce code duplication"
    This reverts commit dfc63b7e140b465d4d4a3ea5926277a6bb661a13.

--- week03/exercise01/heat_stencil_2D_mpi.c
+++ week03/exercise01/heat_stencil_2D_mpi.c
@@ -3,7 +3,21 @@
 #include <time.h>
 #include <mpi.h>
 
-#include "common/common.h"
+typedef double value_t;
+
+#define RESOLUTION 120
+
+#define IDX(i, j, N) ((i)*N + (j))
+
+// -- matrix utilities --
+
+typedef value_t* Matrix;
+
+Matrix createMatrix(int N, int M);
+
+void releaseMatrix(Matrix m);
+
+void printTemperature(Matrix m, int N);
 
 // -- simulation code ---
 
@@ -209,4 +223,69 @@ int main(int argc, char **argv) {
 
   // done
   return EXIT_SUCCESS;
+}
+
+Matrix createMatrix(int N, int M) {
+  return malloc(sizeof(value_t) * N * M);
+}
+
+void releaseMatrix(Matrix m) {
+  free(m);
+}
+
+void printTemperature(Matrix m, int N) {
+  const char *colors = " .-:=+*^X#%@";
+  const int numColors = 12;
+
+  // boundaries for temperature (for simplicity hard-coded)
+  const value_t max = 273 + 30;
+  const value_t min = 273 + 0;
+
+  // set the 'render' resolution
+  int W = RESOLUTION;
+
+  // step size in each dimension
+  int sW = N / W;
+
+  // top border
+  for (int i = 0; i < W + 2; i++) {
+    printf("X");
+  }
+  printf("\n");
+
+  // every line of the plane
+  for (int i = 0; i < W; i++) {
+
+    // left wall
+    printf("X");
+
+    // every tile in the line
+    for (int j = 0; j < W; j++) {
+
+      // get max temperature for this tile (in both x and y direction)
+      value_t max_t = 0;
+      for (int x = sW * i; x < sW * i + sW; x++) {
+        for (int y = sW * j; y < sW * j + sW; y++) {
+          max_t = (max_t < m[IDX(x, y, N)]) ? m[IDX(x, y, N)] : max_t;
+        }
+      }
+      value_t temp = max_t;
+
+      // pick the 'color'
+      int c = ((temp - min) / (max - min)) * numColors;
+      c = (c >= numColors) ? numColors - 1 : ((c < 0) ? 0 : c);
+
+      // print the average temperature
+      printf("%c", colors[c]);
+    }
+
+    // right wall
+    printf("X\n");
+  }
+
+  // bottom border
+  for (int i = 0; i < W + 2; i++) {
+    printf("X");
+  }
+  printf("\n");
 }
