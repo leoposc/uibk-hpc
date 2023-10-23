0178736e362a476aff2353152ed65eb96394f196
LV Informatik <cb761012@login.lcc3.uibk.ac.at>
2023-10-23 08:18:37 +0200

    modified time and size output for script

--- week03/ex1/job.sh
+++ week03/ex1/job.sh
@@ -6,10 +6,23 @@
 #SBATCH --job-name test
 # Redirect output stream to this file
 #SBATCH --output=output.log
-# Maximum number of tasks (=processes) to start in total
-#SBATCH --ntasks=20
+#SBATCH --ntasks=96
 # Maximum number of tasks (=processes) to start per node
+#SBATCH --ntasks-per-node=12
 # Enforce exclusive node allocation, do not share with other jobs
 #SBATCH --exclusive
 
-srun ./heat_stencil_2D_mpi 768
+ns=(192 288 384 480 576 672 768)
+rs=(2 4 8 16 32 96)
+
+module purge
+module load openmpi/3.1.6-gcc-12.2.0-d2gmn55
+
+# mpi
+for n in "${ns[@]}"
+do
+    for r in "${rs[@]}"
+    do
+      mpiexec -np $r ./heat_stencil_2D_parallel $n
+    done
+done
