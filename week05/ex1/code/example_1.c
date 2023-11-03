eff7f644c6fb49af0ccfd636ca6d7126ff1867f5
Peter Burger <peter.burger@student.uibk.ac.at>
2023-11-03 22:23:02 +0100

    fix the bugs in example_1 and example_2

--- week05/ex1/code/example_1.c
+++ week05/ex1/code/example_1.c
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
 	int myrank; /* my rank */
 	const int count = 10;
 	int tag = 42;  /* tag used for all communication */
-	int tag2 = tag + 1; /* extra tag used for what ever you want */
+	// FIXED: removed 'tag2'
 	int good_data = 1;
 	int data[count];   /* data buffers */
 	MPI_Status status; /* status of MPI_Recv() operation */
@@ -22,18 +22,23 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-	for(int i = 0; i < count; i++)
+	// FIXED: added braches to make statement more readable
+	for(int i = 0; i < count; i++) {
 		if(myrank == 0) {
 			data[i] = 399; /* initialize the data for rank 0 process only.  */
-		} else
+		} else {
 			data[i] = i; /* initialize all other data buffers from 0 to count */
+		}
+	}
 
 	if(myrank == 0) {
-		for(int i = 0; i < nprocs; i++) {
+		// FIXED: start at i = 1 (do not send to self)
+		for(int i = 1; i < nprocs; i++) {
 			MPI_Send(&data, count, MPI_INT, i, tag, MPI_COMM_WORLD);
 		}
 	} else {
-		MPI_Recv(&data, count, MPI_INT, 0, tag2, MPI_COMM_WORLD, &status);
+		// FIXED: use 'tag' instead of 'tag2'
+		MPI_Recv(&data, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
--- week05/ex1/code/example_2.c
+++ week05/ex1/code/example_2.c
@@ -28,13 +28,24 @@ int main(int argc, char** argv) {
 	MPI_Type_commit(&newType);
 
 	// 2) Use MPI Sendrecv to perform a ring communication
-	MPI_Sendrecv(sBuf, 1, newType, (rank + 1) % size, 123, rBuf, sizeof(int) * 2, MPI_BYTE,
-	             (1 + size) % size, 123, MPI_COMM_WORLD, &status);
+	// FIXED: replace MPI_BYTE with MPI_INT 
+	// FIXED: replace (rank = 1 + size) with (rank - 1 + size)
+	MPI_Sendrecv(sBuf, 1, newType, (rank + 1) % size, 123, rBuf, sizeof(int) * 2, MPI_INT,
+	             (rank - 1 + size) % size, 123, MPI_COMM_WORLD, &status);
 
 	// 3) Use MPI Send and MPI Recv to perform a ring communication
-	MPI_Send(sBuf, 1, newType, (rank + 1) % size, 456, MPI_COMM_WORLD);
-	MPI_Recv(rBuf, sizeof(int) * 2, MPI_BYTE, (1 + size) % size, 456, MPI_COMM_WORLD,
+	// FIXED: replace MPI_BYTE with MPI_INT
+	// FIXED: replace (rank = 1 + size) with (rank - 1 + size)
+	// FIXED: corrctly orchestate MPI_Send and MPI_Recv to avoid deadlock
+	if (rank % 2) {
+		MPI_Send(sBuf, 1, newType, (rank + 1) % size, 456, MPI_COMM_WORLD);
+		MPI_Recv(rBuf, sizeof(int) * 2, MPI_INT, (rank - 1 + size) % size, 456, MPI_COMM_WORLD,
+				&status);
+	} else {
+		MPI_Recv(rBuf, sizeof(int) * 2, MPI_INT, (rank - 1 + size) % size, 456, MPI_COMM_WORLD,
 	         &status);
+		MPI_Send(sBuf, 1, newType, (rank + 1) % size, 456, MPI_COMM_WORLD);
+	}
 
 	// Say bye bye
 	printf("Signing off, rank %d. \n", rank);
