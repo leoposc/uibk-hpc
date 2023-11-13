56fca1e6045ef45362347a3e9dfb86c02e452537
LV Informatik <cb761012@login.lcc3.uibk.ac.at>
2023-11-13 15:46:56 +0100

    added benchmarks with correct format

--- create_graph.py
+++ create_graph.py
@@ -4,32 +4,36 @@ from plotly.subplots import make_subplots
 import plotly.io as py
 import sys
 
-def parse_numbers_from_file_and_plot(file_path):
+def parse_numbers_from_file_and_plot(directory_path):
     # Initialize an empty dictionary to store the parsed data
     data_dict = {}
     
     # Regular expression pattern to match variables and numbers
     pattern = r'\$!(\w+)\{([\d\.]+), ([\d\.]+)\}'
     
-    # Open the file for reading
-    with open(file_path, 'r') as file:
-        content = file.read()
+    # Iterate over all files in the directory
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
         
-        # Use re.findall to find all matches in the content
-        matches = re.findall(pattern, content)
-        
-        for match in matches:
-            print("match")
-            print(match)
-            var_name = match[0]
+        # Open the file for reading
+        with open(file_path, 'r') as file:
+            content = file.read()
             
-            # Split the comma-separated numbers and convert them to floats
-            numbers = [float(match[1]), float(match[2])]
+            # Use re.findall to find all matches in the content
+            matches = re.findall(pattern, content)
             
-            # Append the numbers to the data_dict
-            if var_name not in data_dict:
-                data_dict[var_name] = []
-            data_dict[var_name].append(numbers)
+            for match in matches:
+                print("match")
+                print(match)
+                var_name = match[0]
+                
+                # Split the comma-separated numbers and convert them to floats
+                numbers = [float(match[1]), float(match[2])]
+                
+                # Append the numbers to the data_dict
+                if var_name not in data_dict:
+                    data_dict[var_name] = []
+                data_dict[var_name].append(numbers)
     
     # Create a Plotly subplot
     fig = make_subplots(rows=1, cols=1)
@@ -90,6 +94,5 @@ def parse_numbers_from_file_and_plot(file_path):
     
 
 # Example usage:
-
-file_path = str(sys.argv[1])
-parse_numbers_from_file_and_plot(file_path)
+directory_path = str(sys.argv[1])
+parse_numbers_from_file_and_plot(directory_path)
