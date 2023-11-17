3910b15d51a8d9a74c6d61d0fda3ed439ab98099
Peter Burger <peter.burger@student.uibk.ac.at>
2023-11-17 12:05:20 +0100

    improve several aspects of the current collective implementation

--- week07/ex01/src/n_body_collective_mpi.c
+++ week07/ex01/src/n_body_collective_mpi.c
@@ -3,133 +3,147 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
-
-#define MIN_DISTANCE_SQR 1.0
-#define G 1.0
-#define FILE_NAME "data.dat"
-#define MAX_CORDINATE 100.0
-#define MAX_MASS 10.0
 
 // #define BENCHMARK
 
+typedef float scalar_t;
+
 typedef struct {
-  float mass;
-  float px;
-  float py;
-  float pz;
-  float vx;
-  float vy;
-  float vz;
+  scalar_t x;
+  scalar_t y;
+  scalar_t z;
+} vector_t;
+
+typedef struct {
+  scalar_t mass;
+  vector_t p;
+  vector_t v;
 } particle_t;
 
-inline float square(float num) {
+// sample a scalar from a uniform distribution
+inline scalar_t runif(scalar_t min, scalar_t max) {
+  return min + (rand() / (scalar_t) RAND_MAX) * (max - min);
+}
+
+// compute the square of a scalar
+inline scalar_t square(scalar_t num) {
   return num * num;
 }
 
-void storePositions(particle_t *partciles, size_t num_particles, FILE* fp) {
+// store the collected positions to the simulation data
+void store_positions(vector_t *positions, size_t num_particles, FILE* fp) {
   for (size_t i = 0; i < num_particles; i++) {
-    fprintf(fp, "%f ", partciles[i].px);
-    fprintf(fp, "%f ", partciles[i].py);
-    fprintf(fp, "%f\n", partciles[i].pz);
+    fprintf(fp, "%f ", positions[i].x);
+    fprintf(fp, "%f ", positions[i].y);
+    fprintf(fp, "%f\n", positions[i].z);
   }
   fprintf(fp, "\n\n");
 }
 
-// create a 1D array of particles
+// create an array of scalars
+scalar_t *create_scalars(size_t n) {
+  return malloc(n * sizeof(scalar_t));
+}
+
+// create an array of 3D vectors
+vector_t *create_vects(size_t n) {
+  return malloc(n * sizeof(vector_t));
+}
+
+// create an array of 3D vectors
 particle_t *create_particles(size_t n) {
   return malloc(n * sizeof(particle_t));
 }
 
 int main(int argc, char* argv[]) {
 	
-  MPI_Init(&argc, &argv);
-
-	MPI_Comm comm;
-	MPI_Comm_dup(MPI_COMM_WORLD, &comm);
-
-	int rank;
-	MPI_Comm_rank(comm, &rank);
-
+  // --- MPI setup ---
+  MPI_Comm comm;
+  int rank;
   int size;
+  MPI_Datatype vect_mpi_type;
+  MPI_Init(&argc, &argv);
+	MPI_Comm_dup(MPI_COMM_WORLD, &comm);
+	MPI_Comm_rank(comm, &rank);
 	MPI_Comm_size(comm, &size);
+  MPI_Type_contiguous(3, MPI_FLOAT, &vect_mpi_type);
+  MPI_Type_commit(&vect_mpi_type);
+  // -----------------
 
+  // --- parameter setup ---
   size_t num_particles;
   size_t time_steps;
   if (argc != 3) {
     num_particles = 2;
     time_steps = 20;
-  }
-  else {
+  } else {
     num_particles = atoi(argv[1]);
     time_steps = atoi(argv[2]);
   }
+  // -----------------------
+
+  // --- global MPI setup ---
+  const size_t ROOT = 0;
+  // ------------------------
+
+  // --- local MPI setup ---
+  const size_t T = time_steps;
+  const size_t R = size;
+  const size_t N = num_particles;
+  const size_t K = N / R;
+  const size_t local_offset = K * rank;
+  // ------------------------
+
+  // --- global simulation constants ---
+  const scalar_t G = 1.0;
+  const scalar_t MASS = 1.0;
+  const scalar_t MAX_COORDINATE = 100.0;
+  const scalar_t MIN_DISTANCE_SQR = 1.0;
+  // ----------------------------
 
 #ifndef BENCHMARK
-  FILE *fp = fopen(FILE_NAME, "w");
+  const char *OUT_FILE = "data.dat";
+  FILE *fp = fopen(OUT_FILE, "w");
   if (fp == NULL) {
     printf("file opening failed\n");
     return EXIT_FAILURE;
   }
 #endif
 
-  // define the particle_t struct type
-  MPI_Datatype particle_type;
-  int blockLengths[7] = {1, 1, 1, 1, 1, 1, 1};
-  MPI_Aint displacements[7];
-  MPI_Datatype types[7] = {MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT};
+  // --- global simulation setup ---
+  scalar_t *global_masses = create_scalars(N);
+  vector_t *global_positions = create_vects(N);
 
-  // calculate the displacements of each struct member
-  MPI_Get_address(&(((particle_t *)0)->mass), &displacements[0]);
-  MPI_Get_address(&(((particle_t *)0)->px), &displacements[1]);
-  MPI_Get_address(&(((particle_t *)0)->py), &displacements[2]);
-  MPI_Get_address(&(((particle_t *)0)->pz), &displacements[3]);
-  MPI_Get_address(&(((particle_t *)0)->vx), &displacements[4]);
-  MPI_Get_address(&(((particle_t *)0)->vy), &displacements[5]);
-  MPI_Get_address(&(((particle_t *)0)->vz), &displacements[6]);
-
-  // adjust the displacements to be relative to the struct address
-  for (int i = 6; i > 0; i--) {
-      displacements[i] -= displacements[0];
-  }
-  displacements[0] = 0;
-
-  MPI_Type_create_struct(7, blockLengths, displacements, types, &particle_type);
-  MPI_Type_commit(&particle_type);
-
-  const size_t ROOT = 0;
-  const size_t T = time_steps;
-  const size_t R = size;
-  const size_t N = num_particles;
-  const size_t K = N / R;
-
-  const size_t offset = K*rank;
-
-  // allocate memory for the collected particles on each rank
-  particle_t *collected_particles = create_particles(N);
-
-  // allocate memory for the local particles on each rank
-  particle_t *local_particles = create_particles(K);
-
-  // initialize the collected particles only on the root node
+  // initialize the collected positions and masses (only on the root node)
   // otherwise there might be problems with the random numbers
   if (rank == ROOT) {
     for (size_t i = 0; i < N; i++) {
-      collected_particles[i].mass = 1.0;
-      collected_particles[i].vx = 0.0;
-      collected_particles[i].vy = 0.0;
-      collected_particles[i].vz = 0.0;
-      collected_particles[i].px = rand() / (1.0 * RAND_MAX) * MAX_CORDINATE;
-      collected_particles[i].py = rand() / (1.0 * RAND_MAX) * MAX_CORDINATE;
-      collected_particles[i].pz = rand() / (1.0 * RAND_MAX) * MAX_CORDINATE;
+      global_masses[i] = MASS;
+      global_positions[i].x = runif(0, MAX_COORDINATE);
+      global_positions[i].y = runif(0, MAX_COORDINATE);
+      global_positions[i].z = runif(0, MAX_COORDINATE);
     }
   }
 
-  // broadcast the initial state to the other ranks
-  MPI_Bcast(collected_particles, N, particle_type, ROOT, comm);
+  // exchange collected states
+  MPI_Bcast(global_masses, N, MPI_FLOAT, ROOT, comm);
+  MPI_Bcast(global_positions, N, vect_mpi_type, ROOT, comm);
+  // ------------------------------
 
-  // on each rank, initialize the local particles
-  memcpy(local_particles, collected_particles + K * rank, K * sizeof(particle_t));
+  // --- local simulation setup ---
+  particle_t *local_particles = create_particles(K);
+  for (size_t k = 0; k < K; k++) {
+    local_particles[k].mass = global_masses[local_offset + k];
+    local_particles[k].p.x = global_positions[local_offset + k].x;
+    local_particles[k].p.y = global_positions[local_offset + k].y;
+    local_particles[k].p.z = global_positions[local_offset + k].z;
+    local_particles[k].v.x = 0.0;
+    local_particles[k].v.x = 0.0;
+    local_particles[k].v.x = 0.0;
+  }
+  // ------------------------------
+
+  vector_t *local_position_buffer = create_vects(K);
 
   // start measuring the time
   clock_t start_time = clock();
@@ -140,7 +154,7 @@ int main(int argc, char* argv[]) {
 #ifndef BENCHMARK   
     // write the current state to the disc 
     if (rank == ROOT) {
-      storePositions(collected_particles, N, fp);
+      store_positions(global_positions, N, fp);
     }
 #endif
 
@@ -148,12 +162,14 @@ int main(int argc, char* argv[]) {
     for (size_t k = 0; k < K; k++) {
 
       // initialize the forces
-      float fx = 0;
-      float fy = 0;
-      float fz = 0;
+      vector_t f = {
+        0,
+        0,
+        0
+      };
 
       // compute the corresponding index for i in the collected particles
-      size_t i = offset + k;
+      size_t i = local_offset + k;
 
       // for every state of the previous particles...
       for (size_t j = 0; j < N; j++) {
@@ -163,16 +179,23 @@ int main(int argc, char* argv[]) {
           continue;
         }
 
-        particle_t p_i = collected_particles[i];
-        particle_t p_j = collected_particles[j];
+        // load the respective masses
+        scalar_t m_i = global_masses[i];
+        scalar_t m_j = global_masses[j];
+
+        // load the respective positions
+        vector_t p_i = global_positions[i];
+        vector_t p_j = global_positions[j];
 
         // compute the displacement between two particles
-        float dx = p_j.px - p_i.px;
-        float dy = p_j.py - p_i.py;
-        float dz = p_j.pz - p_i.pz;
+        vector_t d = {
+          p_j.x - p_i.x,
+          p_j.y - p_i.y,
+          p_j.z - p_i.z
+        };
 
         // compute the squared distance
-        float distance_sqr = square(dx) + square(dy) + square(dz);
+        scalar_t distance_sqr = square(d.x) + square(d.y) + square(d.z);
 
         // avoid division by 0 and reduce the effects of floating point errors
         if (distance_sqr < MIN_DISTANCE_SQR) {
@@ -180,33 +203,38 @@ int main(int argc, char* argv[]) {
         }
 
         // compute the magnitude of the force
-        float f_mag = G * p_i.mass * p_j.mass / distance_sqr;
+        scalar_t f_mag = G * m_i * m_j / distance_sqr;
 
         // compute the regular distance
-        float distance = sqrt(distance_sqr);
+        scalar_t distance = sqrt(distance_sqr);
 
         // update the forces using the equation of gravity
-        fx += f_mag * dx / distance;
-        fy += f_mag * dy / distance;
-        fz += f_mag * dz / distance;
+        f.x += f_mag * d.x / distance;
+        f.y += f_mag * d.y / distance;
+        f.z += f_mag * d.z / distance;
       }
 
-      // update the velocities accordingly
-      local_particles[k].vx += fx / local_particles[k].mass;
-      local_particles[k].vy += fy / local_particles[k].mass;
-      local_particles[k].vz += fz / local_particles[k].mass;
+      // update the local velocities accordingly
+      local_particles[k].v.x += f.x / local_particles[k].mass;
+      local_particles[k].v.y += f.y / local_particles[k].mass;
+      local_particles[k].v.z += f.z / local_particles[k].mass;
 
-      // update the positions accordingly
-      local_particles[k].px += local_particles[k].vx;
-      local_particles[k].py += local_particles[k].vy;
-      local_particles[k].pz += local_particles[k].vz;
+      // update the local positions accordingly
+      local_particles[k].p.x += local_particles[k].v.x;
+      local_particles[k].p.y += local_particles[k].v.y;
+      local_particles[k].p.z += local_particles[k].v.z;
+
+      // set the element in the local position buffer
+      local_position_buffer[k].x = local_particles[k].p.x;
+      local_position_buffer[k].y = local_particles[k].p.y;
+      local_position_buffer[k].z = local_particles[k].p.z;
     }
 
     // gather the local results from all ranks
-    MPI_Gather(local_particles, K, particle_type, collected_particles, K, particle_type, ROOT, comm);
+    MPI_Gather(local_position_buffer, K, vect_mpi_type, global_positions, K, vect_mpi_type, ROOT, comm);
 
     // broadcast the updated collected particles back to the ranks
-    MPI_Bcast(collected_particles, N, particle_type, ROOT, comm);
+    MPI_Bcast(global_positions, N, vect_mpi_type, ROOT, comm);
   }
 
   // print the benchmark information
@@ -218,8 +246,10 @@ int main(int argc, char* argv[]) {
 
   // clean up
   free(local_particles);
-  free(collected_particles);
-  MPI_Type_free(&particle_type);
+  free(local_position_buffer);
+  free(global_masses);
+  free(global_positions);
+  MPI_Type_free(&vect_mpi_type);
 
 #ifndef BENCHMARK
   fclose(fp);
