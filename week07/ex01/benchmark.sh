f19e25565b98082c34f6b62e18bf91284146cb13
Peter Burger <peter.burger@student.uibk.ac.at>
2023-11-17 16:59:23 +0100

    add naive p2p implementation and benchmark

--- week07/ex01/benchmark.sh
+++ week07/ex01/benchmark.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # executables
-es=('n_body_collective_mpi')
+es=('n_body_collective_mpi' 'n_body_p2p_mpi')
 
 # ranks
-rs=(96)
+rs=(1 12 48 96)
 
 # time steps
 ts=(100)
 
 # problem sizes
-ns=(4992)
+ns=(1248 2496 4992 9984)
 
 # results for this exeuction
 out_dir=$(date +%s)
