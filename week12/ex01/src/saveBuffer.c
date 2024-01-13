48e12e16acf3d59c85a0f76fd4be13951e594ea7
Fabian Aster <f.aster@student.uibk.ac.at>
2024-01-13 18:16:08 +0100

    its working on laptop

--- week12/ex01/src/saveBuffer.c
+++ week12/ex01/src/saveBuffer.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    char buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE * size];
     for (int i = 0; i < BUFFER_SIZE; i++) {
         buffer[i] = 'A' + (char)rank;
     }
@@ -22,9 +22,9 @@ int main(int argc, char **argv) {
     MPI_File_open(MPI_COMM_WORLD, "file.txt", MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &file);
 
     for (int i = 0; i < ITERATIONS; i++) {
-        MPI_File_write_all(file, buffer, BUFFER_SIZE, MPI_CHAR, MPI_STATUS_IGNORE);
+        MPI_File_write_at(file, rank*BUFFER_SIZE*sizeof(char), buffer, BUFFER_SIZE*sizeof(char), MPI_CHAR, MPI_STATUS_IGNORE);
         printf("After write Rank %d: %s\n", rank, buffer);
-        MPI_File_read_all(file, buffer, BUFFER_SIZE, MPI_CHAR, MPI_STATUS_IGNORE);
+        MPI_File_read_at(file, rank*BUFFER_SIZE*sizeof(char), buffer, BUFFER_SIZE * sizeof(char), MPI_CHAR, MPI_STATUS_IGNORE);
         printf("After read %d: %s\n", rank, buffer);
     }
     printf("Rank %d: %s\n", rank, buffer);
